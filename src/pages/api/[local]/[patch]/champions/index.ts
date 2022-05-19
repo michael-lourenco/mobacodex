2301d129144b1326ffa66ddd85666ca248a423fc
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-05-19 18:02:39 -0300

    Updated champions

--- src/pages/api/[local]/[patch]/champions/index.ts
+++ src/pages/api/[local]/[patch]/champions/index.ts
@@ -23,14 +23,14 @@ export default async function handler(req, res) {
 
     const url= `./src/backend/data/dragontail/dragontail-${patch}/${patch}/data/${local}/champion.json`
     console.log('URL SEARCHED', url)
-    if(!await existsPath(url)){
-      console.log("Directory does not exist.")
-      res.status(200).json({})
-    } else {  
+    // if(!await existsPath(url)){
+    //   console.log("Directory does not exist.")
+    //   res.status(200).json({})
+    // } else {  
       console.log("Directory exists.")
       const championsData = await import(`../../../../../backend/data/dragontail/dragontail-${patch}/${patch}/data/${local}/champion.json`)
       res.status(200).json(championsData)
-    }
+    //}
   }catch(error){
     console.log(error)
   }
