89b3a8e8622d311a2fae759a9cccb91318083d97
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-05-20 09:03:55 -0300

    Verify url

--- src/pages/api/[local]/[patch]/champions/index.ts
+++ src/pages/api/[local]/[patch]/champions/index.ts
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
   try{
     const { local, patch } = req.query
 
-    const url= `src/backend/data/dragontail/dragontail-${patch}/${patch}/data/${local}/champion.json`
+    const url= `./src/backend/data/dragontail/dragontail-${patch}/${patch}/data/${local}/champion.json`
     
     console.log('URL SEARCHED', url)
     console.log('URL ABSOLUTE', require('path').resolve(url))
