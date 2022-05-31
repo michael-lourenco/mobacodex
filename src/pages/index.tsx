0025218e43d04da2e6d8391883f3cbc9f05db375
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-05-31 17:22:54 -0300

    Update Terms of Services

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">MOBA CODEX</h1>
+            <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">MOBA CODEX API</h1>
           </div>
         
           {session ? (  
@@ -35,7 +35,7 @@ export default function Home() {
               <div className="mt-8 space-y-6">
                 <div>
                   <button onClick = {() => signIn("github")} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Sign in
+                    Sign in with GitHub
                   </button>
                 </div>
               </div>
--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -1,7 +1,22 @@
+import Head from 'next/head'
+
 export default function Terms() {
   return(
     <>
-      <h1>Termos de serviço</h1>
+      <Head>
+        <title>Moba Codex API - Terms of Service</title>
+      </Head>
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div>
+        <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Moba Codex API - Terms of Service </h1>
+        <div className='text-justify'>
+          <h2 className='block text-2xl'>Last modified: May 31, 2022</h2>
+          <p>Thank you for using Moba Codex's API. By accessing or using our API, you are agreeing to the terms below. If there is a conflict between these terms and additional terms applicable to a given API, the additional terms will control for that conflict. Collectively, we refer to the terms below, any additional terms, terms within the accompanying API documentation, and any applicable policies and guidelines as the "Terms." You agree to comply with the Terms and that the Terms control your relationship with us. So please read all the Terms carefully. If you use the API as an interface to, or in conjunction with other Moba Codex products or services, then the terms for those other products or services also apply.</p>
+          <p>Under the Terms, "Moba Codex" means Michael Lourenço, with offices at 96 José Soares Hungria, Street, Jd Mesquita, São Paulo 18213-601, Brasil, unless set forth otherwise in additional terms applicable for a given API. We may refer to "Moba Codex" as "we", "our", or "us" in the Terms.</p>
+          <p>Moba Codex API isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.</p>
+        </div>
+        </div>
+      </div>
     </>
   )
 }
