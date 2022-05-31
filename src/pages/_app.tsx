2cb11b4273368df1f98817e6430db2b104ba1366
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-05-31 10:32:24 -0300

    Add chakra css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,26 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps: { session, ...pageProps} }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ChakraProvider theme={theme}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ChakraProvider>
   ) 
 }
 
