9321191f2e1e03a557d1c8d6f47a1cfb9d11a263
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-06-03 16:02:51 -0300

    Centralized homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <title>Moba Codex API</title>
       </Head>
 
-      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="flex h-screen w-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
             <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">MOBA CODEX API</h1>
