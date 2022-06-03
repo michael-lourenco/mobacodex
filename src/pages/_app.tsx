d1a56d6d4ea325d80cf1e4dfe66cafc241aa67e3
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-06-03 20:14:08 -0300

    Updated typography

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
-import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 
 function MyApp({ Component, pageProps: { session, ...pageProps} }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div className="flex h-screen w-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">MOBA CODEX API</h1>
+            <h1 className="mt-6 text-center text-3xl font-extrabold logo text-gray-900">MOBA CODEX API</h1>
           </div>
         
           {session ? (  
