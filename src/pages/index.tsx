94a8adcfaa480baa6ac389435a43539de9c01cd1
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-06-03 16:23:00 -0300

    Updated typography

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
               <h2 className='block text-center text-2xl'>{session?.user?.name}</h2>
               <div className="mt-8 space-y-6">
               <Link href="/doc-api" >
-                  <a className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Go to API</a>
+                  <a className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">GO TO API</a>
                 </Link>
                 <div>
                   <button onClick = {() => signOut()} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
