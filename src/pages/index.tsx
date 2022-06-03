bd21eda1b1b8b837b4e3e790d4e7781b1469254e
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-06-03 16:35:10 -0300

    Updated typography

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,8 @@ export default function Home() {
             </>
           ):(
             <>
-              <h2 className='block text-center text-1xl'>GET DATA ABOUT CHAMPIONS AND ITEMS OF LOL</h2>
+              <h2 className='block text-center text-1xl text-gray-400 '>GET DATA ABOUT <span className='text-gray-800'> CHAMPIONS </span> AND <span className='text-gray-800'> ITEMS </span> OF <span className='text-gray-800'>LOL</span>
+              </h2>
               <div className="mt-8 space-y-6">
                 <div>
                   <button onClick = {() => signIn("github")} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
